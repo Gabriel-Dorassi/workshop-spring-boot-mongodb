266c966d425ce2e23f97673bfe417ed955aed9dd
Gabriel-Dorassi <dorassi30@gmail.com>
2023-12-08 15:05:08 -0300

    user by id

--- src/main/java/com/gabriel/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/gabriel/workshopmongo/dto/UserDTO.java
@@ -5,7 +5,6 @@ import com.gabriel.workshopmongo.domain.User;
 import java.io.Serializable;
 
 public class UserDTO implements Serializable {
-    private static final long serialVersionUID = 1L;
 
     private String id;
     private String name;
--- src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
@@ -5,12 +5,10 @@ import com.gabriel.workshopmongo.dto.UserDTO;
 import com.gabriel.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,12 +19,18 @@ public class  UserResource {
     @Autowired
     private UserService service;
 
-    @RequestMapping(method = RequestMethod.GET )
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<UserDTO>> findAll() {
 
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
 
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
